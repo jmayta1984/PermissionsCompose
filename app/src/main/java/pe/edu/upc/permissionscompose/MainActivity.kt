a986edb728e417776da2dba4dd6f3611892d01d6
Jorge Mayta <jmayta17@hotmai.com>
2021-10-23 13:45:00 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/upc/permissionscompose/MainActivity.kt
@@ -0,0 +1,82 @@
+package pe.edu.upc.permissionscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
+import pe.edu.upc.permissionscompose.ui.theme.PermissionsComposeTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PermissionsComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    FeatureThatRequiresCameraPermission()
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun FeatureThatRequiresCameraPermission() {
+    var doNotShowRationale by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    var cameraPermissionState =
+        rememberPermissionState(permission = android.Manifest.permission.CAMERA)
+
+
+    PermissionRequired(
+        permissionState = cameraPermissionState,
+        permissionNotGrantedContent = {
+            if (doNotShowRationale) {
+                Text("Feature not available")
+            } else {
+                Column {
+                    Text("Camera is important for this app. Please grant ther permission.")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(onClick = {
+                            cameraPermissionState.launchPermissionRequest()
+                        }) {
+                            Text("OK!")
+                        }
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Button(onClick = {
+                            doNotShowRationale = true
+                        }) {
+                            Text("Nope")
+                        }
+                    }
+                }
+            }
+        },
+        permissionNotAvailableContent = {
+            Column {
+                Text("Camera permission denied.")
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }) {
+        Text("Camera Permission Granted")
+    }
+}
